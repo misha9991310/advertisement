2c61459f27aaff0b784e0c13fdb2981970e18add
misha999210 <miplik@yandex.ru>
2023-09-27 12:27:29 +0700

    Создана структура проекта,подключена база данных, добавлены модель пользователей, регистрация и аутентификация.

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.routing import APIRouter
+import uvicorn
+
+from api.handlers import user_router
+from api.login_handler import login_router
+
+app = FastAPI(title='advertisement')
+
+
+main_api_router = APIRouter()
+
+
+main_api_router.include_router(user_router, prefix="/user", tags=["user"])
+main_api_router.include_router(login_router, prefix="/login", tags=["login"])
+app.include_router(main_api_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
